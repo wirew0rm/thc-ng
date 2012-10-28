2271bb2a349574051c6f135187003ea180fb5cf9
Jonathan Sieber <jonathan_sieber@yahoo.de>
2012-10-28 21:09:05 +0100

    PadKontrol support

--- app.js
+++ app.js
@@ -33,13 +33,37 @@ app.get(/ambience\/cinema\/?(.*)/, ambience.cinema);
 app.get('/ambience/color', ambience.color);
 app.get('/ambience/color/*', ambience.color);
 //app.get('/audio/*', audio.play);
-app.get('/', routes.index);
+app.get('/', ambience.color);
+
+var PadKontrol = require('./lib/padkontrol')
+var padkontrol = new PadKontrol(2, 2)
+//padkontrol.open(2, 2);
+padkontrol.on('pad_timer', function(pad, pressedTime, vel) {
+  if (pad < 8) {
+    var color = light.getMainColor();
+    var speed = vel / 255 * pressedTime;
+
+    if (pad == 0) color = color.darken(speed);
+    if (pad == 4) color = color.lighten(speed);
+    if (pad == 1) color = color.red(-speed, true);
+    if (pad == 5) color = color.red(speed, true);
+    if (pad == 2) color = color.green(-speed, true);
+    if (pad == 6) color = color.green(speed, true);
+    if (pad == 3) color = color.blue(-speed, true);
+    if (pad == 7) color = color.blue(speed, true);
+    
+    light.setMainColor(color);
+  }
+})
+
+padkontrol.on('pad', function(pad, pressed, vel) {
+  if (pressed && pad == 8) audiosample.play('miau/miau.wav', vel / 127.0);
+})
 
 var server = http.createServer(app);
 var io = require('socket.io');
 var socket = io.listen(server);
 
-console.log("foo")
 light.init(socket.of('/ambience'));
 
 server.listen(app.get('port'), function() {
